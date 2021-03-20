206fd85b397cf43c924ba8ac0363ace9ee323b28
Umut Tepe <tepeumut1@gmail.com>
2021-03-21 02:19:21 +0300

    Added User Service

---
+++ src/main/java/com/umuttepe/restexample/service/UserService.java
@@ -0,0 +1,36 @@
+package com.umuttepe.restexample.service;
+
+import com.umuttepe.restexample.entity.User;
+import com.umuttepe.restexample.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+
+public class UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUser(Long id) {
+        return userRepository.getOne(id);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
