b6e6c7b92f905ea200b1ede76cb5aa29f07f9d3b
Umut Tepe <tepeumut1@gmail.com>
2021-03-21 14:07:49 +0300

    Added User DTO

---
+++ src/main/java/com/umuttepe/restexample/dto/user/UserDto.java
@@ -0,0 +1,12 @@
+package com.umuttepe.restexample.dto.user;
+
+import java.util.Date;
+
+public class UserDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String username;
+    private Date createdAt;
+}
