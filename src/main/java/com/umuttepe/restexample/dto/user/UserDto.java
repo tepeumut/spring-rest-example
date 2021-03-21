44402e611abdbdb476c6600bb972e43d6ce18b92
Umut Tepe <tepeumut1@gmail.com>
2021-03-21 14:18:58 +0300

    Updated User DTO

--- src/main/java/com/umuttepe/restexample/dto/user/UserDto.java
+++ src/main/java/com/umuttepe/restexample/dto/user/UserDto.java
@@ -1,7 +1,14 @@
 package com.umuttepe.restexample.dto.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
     private Long id;
     private String firstName;
