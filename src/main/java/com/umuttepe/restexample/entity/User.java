ceabea97686152bc5f87359117553a2a648c3028
Umut Tepe <tepeumut1@gmail.com>
2021-03-21 14:20:31 +0300

    Updated User Entity

--- src/main/java/com/umuttepe/restexample/entity/User.java
+++ src/main/java/com/umuttepe/restexample/entity/User.java
@@ -1,6 +1,8 @@
 package com.umuttepe.restexample.entity;
 
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -27,9 +29,11 @@ public class User implements Serializable {
     @Column(name = "username")
     private String username;
 
-    @Column(name = "created_at")
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
     private Date createdAt;
 
     @Column(name = "updated_at")
+    @UpdateTimestamp
     private Date updatedAt;
 }
